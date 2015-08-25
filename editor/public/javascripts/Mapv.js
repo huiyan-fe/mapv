6c2208eeaf0e2c5b9f779488a9a29c1cca702819
hinikai <hinikai@gmail.com>
2015-08-25 20:55:18 +0800

    update

--- editor/public/javascripts/Mapv.js
+++ editor/public/javascripts/Mapv.js
@@ -911,7 +911,7 @@ util.extend(Layer.prototype, {
             return false;
         }
 
-        if (this.getContext == '2d') {
+        if (this.getContext() == '2d') {
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         }
 
