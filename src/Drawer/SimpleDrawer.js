6447026e566d94abe0110db803a314b43498ddaa
hinikai <hinikai@gmail.com>
2015-11-04 22:33:16 +0800

    add webgl polyline

--- src/Drawer/SimpleDrawer.js
+++ src/Drawer/SimpleDrawer.js
@@ -10,8 +10,13 @@ function SimpleDrawer() {
 util.inherits(SimpleDrawer, Drawer);
 
 SimpleDrawer.prototype.drawMap = function(time) {
+    var dataType = this.getLayer().getDataType();
     if (this.getLayer().getContext() === 'webgl') {
-        this.drawWebglMap();
+        if (dataType === 'polyline') { // 画线
+            this.drawWebglPolyline();
+        } else {
+            this.drawWebglPoint();
+        }
         return;
     }
 
@@ -25,7 +30,6 @@ SimpleDrawer.prototype.drawMap = function(time) {
 
     var radius = this.getRadius();
 
-    var dataType = this.getLayer().getDataType();
 
     if (dataType === 'polyline' || dataType === 'polygon') { // 画线或面
 
@@ -200,7 +204,7 @@ SimpleDrawer.prototype.drawAnimation = function() {
 }
 
 // 使用webgl来绘点，支持更大数据量的点
-SimpleDrawer.prototype.drawWebglMap = function() {
+SimpleDrawer.prototype.drawWebglPoint = function() {
 
     var data = this.getLayer().getData();
 
@@ -311,4 +315,103 @@ SimpleDrawer.prototype.drawWebglMap = function() {
         colored[2] / 255,
         colored[3] / 255);
     gl.drawArrays(gl.POINTS, 0, n);
+}
+
+// 使用webgl来绘线，支持更大数据量的线
+SimpleDrawer.prototype.drawWebglPolyline = function() {
+    var data = this.getLayer().getData();
+
+    if (!data) {
+        return;
+    }
+
+    var gl = this.getCtx();
+
+    var vs, fs, vs_s, fs_s;
+
+    vs = gl.createShader(gl.VERTEX_SHADER);
+    fs = gl.createShader(gl.FRAGMENT_SHADER);
+
+    vs_s = [
+        'attribute vec4 a_Position;',
+        'void main() {',
+        'gl_Position = a_Position;',
+        'gl_PointSize = 30.0;',
+        '}'
+    ].join('');
+
+    fs_s = [
+        'precision mediump float;',
+        'uniform vec4 u_FragColor;',
+        'void main() {',
+        'gl_FragColor = u_FragColor;',
+        '}'
+    ].join('');
+
+    var program = gl.createProgram();
+    gl.shaderSource(vs, vs_s);
+    gl.compileShader(vs);
+    gl.shaderSource(fs, fs_s);
+    gl.compileShader(fs);
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+
+    //gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var halfCanvasWidth = gl.canvas.width / 2;
+    var halfCanvasHeight = gl.canvas.height / 2;
+
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    var uFragColor = gl.getUniformLocation(program, 'u_FragColor');
+
+    var tmpCanvas = document.createElement('canvas');
+    var tmpCtx = tmpCanvas.getContext('2d');
+    tmpCanvas.width = 1;
+    tmpCanvas.height = 1;
+    tmpCtx.fillStyle = this.getDrawOptions().strokeStyle || 'red';
+    tmpCtx.fillRect(0, 0, 1, 1);
+    var colored = tmpCtx.getImageData(0, 0, 1, 1).data;
+
+    gl.uniform4f(uFragColor,
+        colored[0] / 255,
+        colored[1] / 255,
+        colored[2] / 255,
+        colored[3] / 255);
+
+    gl.lineWidth(this.getDrawOptions().lineWidth || 1);
+
+    for (var i = 0, len = data.length; i < len; i++) {
+        var geo = data[i].pgeo;
+
+        var verticesData = [];
+
+        for (var j = 0; j < geo.length; j++) {
+            var item = geo[j];
+
+            var x = (item[0] - halfCanvasWidth) / halfCanvasWidth;
+            var y = (halfCanvasHeight - item[1]) / halfCanvasHeight;
+            verticesData.push(x, y);
+        }
+        var vertices = new Float32Array(verticesData);
+        // Write date into the buffer object
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        gl.drawArrays(gl.LINE_STRIP, 0, geo.length);
+    }
 }
