e55dd505bba8c0e089a2cab7556bf442618a1f6b
tangshuyuan <tangziyuan@haizhi.com>
2015-12-02 21:10:19 +0800

    优化pinch缩放，使放大的区域能够显示在viewport的中心

--- editor/public/javascripts/Mapv.js
+++ editor/public/javascripts/Mapv.js
@@ -942,6 +942,7 @@ function Mapv(options) {
 
     this._layers = [];
     //this._initDrawScale();
+    this._fixPinchZoom();
 
     this.notify('drawTypeControl');
 }
@@ -965,6 +966,42 @@ Mapv.prototype.drawTypeControl_changed = function () {
             this.getMap().removeControl(this.drawTypeControl);
         }
     }
+};
+
+// 执行pinch手势操作后，将地图的中心点改为两个触摸点的中心点，
+// 使放大的区域能够显示在viewport的中心
+Mapv.prototype._fixPinchZoom = function () {
+    var bmap = this.getMap();
+    var _zoom = bmap.getZoom();
+    var _touchMidPoint;
+    var _offset = bmap.getContainer().getBoundingClientRect();
+
+    bmap.addEventListener('touchstart', function (e) {
+        if (e.targetTouches.length == 2) {
+            var touches = e.targetTouches;
+
+            var middlePoint = {
+                x: (touches[0].clientX + touches[1].clientX) / 2 - _offset.left,
+                y: (touches[0].clientY + touches[1].clientY) / 2 - _offset.top
+            };
+
+            _touchMidPoint = bmap.pixelToPoint(middlePoint);
+        }
+    });
+
+    bmap.addEventListener('touchcancel', function (e) {
+        _touchMidPoint = null;
+    });
+
+    bmap.addEventListener('zoomend', function (e) {
+        var newZoom = bmap.getZoom();
+        if (newZoom > _zoom && _touchMidPoint) {
+            // 放大时才修改中心点
+            bmap.panTo(_touchMidPoint);
+        }
+        _zoom = newZoom;
+        _touchMidPoint = null;
+    });
 };
 /**
  * 一直覆盖在当前地图视野的Canvas对象
