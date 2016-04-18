b14feab0cb0492ba5d5dc20dca30217c288a4b27
zmofei <13761509829@163.com>
2016-04-18 20:49:54 +0800

    support jax & react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	  entry: './dev/es6/index.jsx.js',
+	  output: { path: './static/js/', filename: 'index.js' },
+	  module: {
+		    loaders: [
+		{
+			test: /.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}
+		]
+	},
+};
