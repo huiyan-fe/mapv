c8f415c6a1d98f8b288f41be566947225dc7dab0
hinikai <hinikai@gmail.com>
2016-04-18 19:06:54 +0800

    add csv

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ export {default as googleMapLayer} from "./src/google-map/Layer";
 
 export {default as DataSet} from "./src/data/DataSet";
 export {default as geojson} from "./src/data/geojson";
-
+export {default as csv} from "./src/data/csv";
