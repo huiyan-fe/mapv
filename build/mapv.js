4ef8520926cb3ca058e09c8197678c5d2d779d62
nikai <nikai@baidu.com>
2020-03-03 09:27:08 +0800

    同步代码

--- build/mapv.js
+++ build/mapv.js
@@ -4,7 +4,7 @@
 	(factory((global.mapv = global.mapv || {})));
 }(this, (function (exports) { 'use strict';
 
-var version = "2.0.50";
+var version = "2.0.55";
 
 /**
  * @author kyle / http://nikai.us/
@@ -1198,7 +1198,7 @@ var drawGrid = {
             for (var i = 0; i < data.length; i++) {
                 var coordinates = data[i].geometry._coordinates || data[i].geometry.coordinates;
                 var gridKey = coordinates.join(',');
-                grids[gridKey] = ~~(data[i].count || 1);
+                grids[gridKey] = data[i].count || 1;
             }
             for (var _gridKey in grids) {
                 _gridKey = _gridKey.split(',');
@@ -3053,10 +3053,10 @@ function CanvasLayer(options) {
 }
 
 var global$3 = typeof window === 'undefined' ? {} : window;
+var BMap$1 = global$3.BMap || global$3.BMapGL;
+if (BMap$1) {
 
-if (global$3.BMap) {
-
-    CanvasLayer.prototype = new BMap.Overlay();
+    CanvasLayer.prototype = new BMap$1.Overlay();
 
     CanvasLayer.prototype.initialize = function (map) {
         this._map = map;
@@ -3064,10 +3064,17 @@ if (global$3.BMap) {
         canvas.style.cssText = "position:absolute;" + "left:0;" + "top:0;" + "z-index:" + this.zIndex + ";user-select:none;";
         canvas.style.mixBlendMode = this.mixBlendMode;
         this.adjustSize();
-        map.getPanes()[this.paneName].appendChild(canvas);
+        var pane = map.getPanes()[this.paneName];
+        if (!pane) {
+            pane = map.getPanes().floatShadow;
+        }
+        pane.appendChild(canvas);
         var that = this;
         map.addEventListener('resize', function () {
             that.adjustSize();
+            that._draw();
+        });
+        map.addEventListener('update', function () {
             that._draw();
         });
         /*
@@ -3075,6 +3082,11 @@ if (global$3.BMap) {
             that._draw();
         });
         */
+        if (this.options.updateImmediate) {
+            setTimeout(function () {
+                that._draw();
+            }, 100);
+        }
         return this.canvas;
     };
 
@@ -3096,10 +3108,14 @@ if (global$3.BMap) {
 
     CanvasLayer.prototype.draw = function () {
         var self = this;
-        clearTimeout(self.timeoutID);
-        self.timeoutID = setTimeout(function () {
+        if (this.options.updateImmediate) {
             self._draw();
-        }, 15);
+        } else {
+            clearTimeout(self.timeoutID);
+            self.timeoutID = setTimeout(function () {
+                self._draw();
+            }, 15);
+        }
     };
 
     CanvasLayer.prototype._draw = function () {
@@ -5268,6 +5284,9 @@ var BaseLayer = function () {
     return BaseLayer;
 }();
 
+var global$4 = typeof window === 'undefined' ? {} : window;
+var BMap$2 = global$4.BMap || global$4.BMapGL;
+
 var AnimationLayer = function (_BaseLayer) {
     inherits(AnimationLayer, _BaseLayer);
 
@@ -5335,7 +5354,22 @@ var AnimationLayer = function (_BaseLayer) {
     }, {
         key: "transferToMercator",
         value: function transferToMercator() {
-            var projection = this.map.getMapType().getProjection();
+            var map = this.map;
+            var mapType = map.getMapType();
+            var projection;
+            if (mapType.getProjection) {
+                projection = mapType.getProjection();
+            } else {
+                projection = {
+                    lngLatToPoint: function lngLatToPoint(point) {
+                        var mc = map.lnglatToMercator(point.lng, point.lat);
+                        return {
+                            x: mc[0],
+                            y: mc[1]
+                        };
+                    }
+                };
+            }
 
             if (this.options.coordType !== 'bd09mc') {
                 var data = this.dataSet.get();
@@ -5358,16 +5392,33 @@ var AnimationLayer = function (_BaseLayer) {
             }
             //clear(ctx);
             var map = this.map;
+            var projection;
+            var mcCenter;
+            if (map.getMapType().getProjection) {
+                projection = map.getMapType().getProjection();
+                mcCenter = projection.lngLatToPoint(map.getCenter());
+            } else {
+                mcCenter = {
+                    x: map.getCenter().lng,
+                    y: map.getCenter().lat
+                };
+                projection = {
+                    lngLatToPoint: function lngLatToPoint(point) {
+                        var mc = map.lnglatToMercator(point.lng, point.lat);
+                        return {
+                            x: mc[0],
+                            y: mc[1]
+                        };
+                    }
+                };
+            }
             var zoomUnit;
-            var projection = map.getMapType().getProjection();
             if (projection.getZoomUnits) {
                 zoomUnit = projection.getZoomUnits(map.getZoom());
             } else {
                 zoomUnit = Math.pow(2, 18 - map.getZoom());
             }
-
-            var mcCenter = projection.lngLatToPoint(map.getCenter());
-            var nwMc = new BMap.Pixel(mcCenter.x - map.getSize().width / 2 * zoomUnit, mcCenter.y + map.getSize().height / 2 * zoomUnit); //左上角墨卡托坐标
+            var nwMc = new BMap$2.Pixel(mcCenter.x - map.getSize().width / 2 * zoomUnit, mcCenter.y + map.getSize().height / 2 * zoomUnit); //左上角墨卡托坐标
 
             clear(ctx);
 
@@ -5533,6 +5584,9 @@ var AnimationLayer = function (_BaseLayer) {
  * @author kyle / http://nikai.us/
  */
 
+var global$5 = typeof window === 'undefined' ? {} : window;
+var BMap$3 = global$5.BMap || global$5.BMapGL;
+
 var Layer = function (_BaseLayer) {
     inherits(Layer, _BaseLayer);
 
@@ -5556,6 +5610,7 @@ var Layer = function (_BaseLayer) {
         var canvasLayer = _this.canvasLayer = new CanvasLayer({
             map: map,
             context: _this.context,
+            updateImmediate: options.updateImmediate,
             paneName: options.paneName,
             mixBlendMode: options.mixBlendMode,
             enableMassClear: options.enableMassClear,
@@ -5643,7 +5698,24 @@ var Layer = function (_BaseLayer) {
             if (!dataSet) {
                 dataSet = this.dataSet;
             }
-            var projection = this.map.getMapType().getProjection();
+
+            var map = this.map;
+
+            var mapType = map.getMapType();
+            var projection;
+            if (mapType.getProjection) {
+                projection = mapType.getProjection();
+            } else {
+                projection = {
+                    lngLatToPoint: function lngLatToPoint(point) {
+                        var mc = map.lnglatToMercator(point.lng, point.lat);
+                        return {
+                            x: mc[0],
+                            y: mc[1]
+                        };
+                    }
+                };
+            }
 
             if (this.options.coordType !== 'bd09mc') {
                 var data = dataSet.get();
@@ -5679,7 +5751,26 @@ var Layer = function (_BaseLayer) {
 
             var map = this.canvasLayer._map;
 
-            var projection = map.getMapType().getProjection();
+            var projection;
+            var mcCenter;
+            if (map.getMapType().getProjection) {
+                projection = map.getMapType().getProjection();
+                mcCenter = projection.lngLatToPoint(map.getCenter());
+            } else {
+                mcCenter = {
+                    x: map.getCenter().lng,
+                    y: map.getCenter().lat
+                };
+                projection = {
+                    lngLatToPoint: function lngLatToPoint(point) {
+                        var mc = map.lnglatToMercator(point.lng, point.lat);
+                        return {
+                            x: mc[0],
+                            y: mc[1]
+                        };
+                    }
+                };
+            }
             var zoomUnit;
             if (projection.getZoomUnits) {
                 zoomUnit = projection.getZoomUnits(map.getZoom());
@@ -5687,8 +5778,7 @@ var Layer = function (_BaseLayer) {
                 zoomUnit = Math.pow(2, 18 - map.getZoom());
             }
 
-            var mcCenter = projection.lngLatToPoint(map.getCenter());
-            var nwMc = new BMap.Pixel(mcCenter.x - map.getSize().width / 2 * zoomUnit, mcCenter.y + map.getSize().height / 2 * zoomUnit); //左上角墨卡托坐标
+            var nwMc = new BMap$3.Pixel(mcCenter.x - map.getSize().width / 2 * zoomUnit, mcCenter.y + map.getSize().height / 2 * zoomUnit); //左上角墨卡托坐标
 
             var context = this.getContext();
 
@@ -5762,7 +5852,7 @@ var Layer = function (_BaseLayer) {
 
             this.processData(data);
 
-            var nwPixel = map.pointToPixel(new BMap.Point(0, 0));
+            var nwPixel = map.pointToPixel(new BMap$3.Point(0, 0));
 
             if (self.options.unit == 'm') {
                 if (self.options.size) {
@@ -6036,9 +6126,9 @@ function CanvasLayer$2(opt_options) {
   }
 }
 
-var global$4 = typeof window === 'undefined' ? {} : window;
+var global$6 = typeof window === 'undefined' ? {} : window;
 
-if (global$4.google && global$4.google.maps) {
+if (global$6.google && global$6.google.maps) {
 
   CanvasLayer$2.prototype = new google.maps.OverlayView();
 
@@ -6079,8 +6169,8 @@ if (global$4.google && global$4.google.maps) {
    * @return {number} The browser-defined id for the requested callback.
    * @private
    */
-  CanvasLayer$2.prototype.requestAnimFrame_ = global$4.requestAnimationFrame || global$4.webkitRequestAnimationFrame || global$4.mozRequestAnimationFrame || global$4.oRequestAnimationFrame || global$4.msRequestAnimationFrame || function (callback) {
-    return global$4.setTimeout(callback, 1000 / 60);
+  CanvasLayer$2.prototype.requestAnimFrame_ = global$6.requestAnimationFrame || global$6.webkitRequestAnimationFrame || global$6.mozRequestAnimationFrame || global$6.oRequestAnimationFrame || global$6.msRequestAnimationFrame || function (callback) {
+    return global$6.setTimeout(callback, 1000 / 60);
   };
 
   /**
@@ -6092,7 +6182,7 @@ if (global$4.google && global$4.google.maps) {
    * @param {number=} requestId The id of the frame request to cancel.
    * @private
    */
-  CanvasLayer$2.prototype.cancelAnimFrame_ = global$4.cancelAnimationFrame || global$4.webkitCancelAnimationFrame || global$4.mozCancelAnimationFrame || global$4.oCancelAnimationFrame || global$4.msCancelAnimationFrame || function (requestId) {};
+  CanvasLayer$2.prototype.cancelAnimFrame_ = global$6.cancelAnimationFrame || global$6.webkitCancelAnimationFrame || global$6.mozCancelAnimationFrame || global$6.oCancelAnimationFrame || global$6.msCancelAnimationFrame || function (requestId) {};
 
   /**
    * Sets any options provided. See CanvasLayerOptions for more information.
@@ -6259,7 +6349,7 @@ if (global$4.google && global$4.google.maps) {
 
     // cease canvas update callbacks
     if (this.requestAnimationFrameId_) {
-      this.cancelAnimFrame_.call(global$4, this.requestAnimationFrameId_);
+      this.cancelAnimFrame_.call(global$6, this.requestAnimationFrameId_);
       this.requestAnimationFrameId_ = null;
     }
   };
@@ -6384,7 +6474,7 @@ if (global$4.google && global$4.google.maps) {
    */
   CanvasLayer$2.prototype.scheduleUpdate = function () {
     if (this.isAdded_ && !this.requestAnimationFrameId_) {
-      this.requestAnimationFrameId_ = this.requestAnimFrame_.call(global$4, this.requestUpdateFunction_);
+      this.requestAnimationFrameId_ = this.requestAnimFrame_.call(global$6, this.requestUpdateFunction_);
     }
   };
 }
